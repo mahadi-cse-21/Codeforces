4beb1697bcdcc6b82605dd14da1371097b7a55a1
mahadi-cse-21 <mahadi.cse.21@gmail.com>
2025-03-11 19:53:23 +0600

    way too long word

---
+++ A_Way_Too_Long_Words.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+        string s;
+        cin>>s;
+        if(s.size()>10) cout<<s[0]<<s.size()-2<<s[s.size()-1]<<endl;
+        else cout<<s<<endl;
+    }
+}
